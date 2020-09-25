56cdc326d1eba5db0363c3c13b5349965458ccd8
Hawen <Hawen@DESKTOP-8I0SOJF>
2020-09-25 16:19:46 +0200

    el adios

--- src/Hola.java
+++ src/Hola.java
@@ -3,7 +3,7 @@ public class Hola {
     public static void main (String [] args){
         String hi = "YEEEp!!!";
         String hi2 = "ASereje ja deje";
-        String bye = "asdadsa";
+        String bye1 = "asdadsa";
         System.out.println(hi);
         
     }
