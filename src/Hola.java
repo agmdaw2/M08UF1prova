0602244f9481f630e6ab3e49776bd4c4f8ae21ce
Hawen <Hawen@DESKTOP-8I0SOJF>
2020-09-25 16:18:05 +0200

    nuevo hi

--- src/Hola.java
+++ src/Hola.java
@@ -2,7 +2,7 @@
 public class Hola {
     public static void main (String [] args){
         String hi = "YEEEp!!!";
-        
+        String hi2 = "ASereje ja deje";
         System.out.println(hi);
         
     }
